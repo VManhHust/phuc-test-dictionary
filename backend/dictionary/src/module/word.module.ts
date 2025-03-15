5720dc5f73d80dd5cf10d06c85032707413c9c0c
manhnv9 <manhnv9@tnteco.vn>
2025-03-15 15:19:56 +0700

    add property count

--- backend/dictionary/src/module/word.module.ts
+++ backend/dictionary/src/module/word.module.ts
@@ -4,10 +4,13 @@ import {WordController} from '../controller/word.controller';
 import {WordService} from '../service/word.service';
 import {Word} from '../entity/word.entity';
 import {PaginationModule} from "./pagination.module";
+import {Definition} from "../entity/definition.entity";
+import {Synonym} from "../entity/synonym.entity";
+import {Etymology} from "../entity/etymology.entity";
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Word]),
+    imports: [TypeOrmModule.forFeature([Word, Definition, Synonym, Etymology]),
         PaginationModule],
     controllers: [WordController],
     providers: [WordService],
